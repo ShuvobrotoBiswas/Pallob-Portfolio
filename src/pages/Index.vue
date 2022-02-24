f4da072f6dbbb05c2c30c8f4372684dab1a8d618
SHUVOBROTO BISWAS <93680258+ShuvobrotoBiswas@users.noreply.github.com>
2022-02-24 16:13:33 +0600

    update
    Updated

---
+++ src/pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-h1">
+    Hi Pallob
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+});
+</script>
