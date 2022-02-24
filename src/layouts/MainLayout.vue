879e54294b38cb2bb5e37f87a7f3e8b43f734f5b
SHUVOBROTO BISWAS <93680258+ShuvobrotoBiswas@users.noreply.github.com>
2022-02-24 16:55:21 +0600

    Update MainLayout.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,7 +11,7 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title> Pallob Vau </q-toolbar-title>
+        <q-toolbar-title> Pallob_Vau </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
