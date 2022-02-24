c0d65bd57569a6cbffc91c88ff54aabfac267d4a
SHUVOBROTO BISWAS <93680258+ShuvobrotoBiswas@users.noreply.github.com>
2022-02-24 16:49:08 +0600

    Updated
    Updated

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="text-h3 q-pa-md absolute-center">
-    Hey Man
-    <q-btn
+  <div class="q-pa-md justify-center items-center">
+    <q-img
+      src="~assets/img1.jpeg"
+      spinner-color="white"
+      style="height: 400px; max-width: 600px"
+      img-class="my-custom-image"
+      class="rounded-borders"
+    >
+      <div class="absolute-bottom text-subtitle1 text-center">
+        Hey Man <br>
+        Welcome to my Website <br>
+        <q-btn
           color="black"
-          icon="collections_bookmark"
-          label="My Facebook"
+          icon="facebook"
+          label="Checkout my facebook"
           href="https://www.facebook.com/Pallob.Vau71"
           />
+      </div>
+    </q-img>
   </div>
 </template>
 
